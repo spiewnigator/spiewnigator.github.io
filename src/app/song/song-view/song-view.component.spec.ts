dcd95f19bd5562c6177272df1263ff8255480fb5
wojtas1411 <wojtas.t.1997@gmail.com>
2021-08-08 12:54:30 +0200

    setup testing

--- src/app/song/song-view/song-view.component.spec.ts
+++ src/app/song/song-view/song-view.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { SongProviderService } from 'src/app/service/song-provider.service';
 
 import { SongViewComponent } from './song-view.component';
 
@@ -6,9 +9,18 @@ describe('SongViewComponent', () => {
   let component: SongViewComponent;
   let fixture: ComponentFixture<SongViewComponent>;
 
+  const songProviderSpy = jasmine.createSpyObj<SongProviderService>('songProviderSpy', ['getOne']);
+  songProviderSpy.getOne.and.returnValue(of({title: 'Title', content: []}))
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SongViewComponent ]
+      declarations: [ SongViewComponent ],
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        {provide: SongProviderService, useValue: songProviderSpy}
+      ]
     })
     .compileComponents();
   });
