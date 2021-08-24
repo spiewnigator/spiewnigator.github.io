4f1d8209ebb471af84b79c9836bd5b8f55fd43fb
Wojciech Taisner <wojtas.t.1997@gmail.com>
2021-08-24 19:32:33 +0200

    Implement song structure (#1)
    * implementation: song content parsing
    
    * setup testing
    
    * finalize test setup

--- src/app/song/song-list/song-list.component.spec.ts
+++ src/app/song/song-list/song-list.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SongProviderService } from 'src/app/service/song-provider.service';
 
 import { SongListComponent } from './song-list.component';
+
+import { of } from 'rxjs';
 
 describe('SongListComponent', () => {
   let component: SongListComponent;
   let fixture: ComponentFixture<SongListComponent>;
 
+  const songProviderSpy = jasmine.createSpyObj<SongProviderService>('songProviderSpy', ['getAll']);
+  songProviderSpy.getAll.and.returnValue(of([]))
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SongListComponent ]
+      declarations: [SongListComponent],
+      providers: [
+        {provide: SongProviderService, useValue: songProviderSpy}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
