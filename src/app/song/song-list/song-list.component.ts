260cd0f56e296c8fc9985e1ca91c93514981f5fa
wojtas1411 <wojtas.t.1997@gmail.com>
2021-09-03 23:13:01 +0200

    add search field to songs list

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map, debounceTime } from 'rxjs/operators';
 import { Song } from 'src/app/model/song';
 import { SongProviderService } from 'src/app/service/song-provider.service';
+
+function songMatches(song: Song, term: string): boolean {
+  const t = term.toLocaleLowerCase();
+
+  return t === '' ||
+    song.title.toLocaleLowerCase().includes(t) ||
+    !!song.author?.toLocaleLowerCase().includes(t);
+}
 
 @Component({
   selector: 'app-song-list',
@@ -9,12 +20,32 @@ import { SongProviderService } from 'src/app/service/song-provider.service';
 })
 export class SongListComponent implements OnInit {
 
-  public songs: Song[] = [];
+  public readonly searchControl = new FormControl('');
+
+  private readonly _searchSubject$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  private readonly _songs$: BehaviorSubject<Song[]> = new BehaviorSubject<Song[]>([]);
+
+  public readonly songs$: Observable<Song[]> = combineLatest(
+    [this._songs$.asObservable(), this._searchSubject$.asObservable()]
+  ).pipe(
+    map(([songs, searchTerm]) => songs.filter(s => songMatches(s, searchTerm)))
+  );
 
   constructor(private readonly songProvider: SongProviderService) { }
 
   ngOnInit(): void {
-    this.songProvider.getAll().subscribe(data => this.songs = data)
+    this.songProvider.getAll().subscribe(
+      data => this._songs$.next(data),
+      error => this._songs$.error(error)
+    );
+
+    this.searchControl.valueChanges.pipe(
+      debounceTime(200)
+    ).subscribe(
+      value => this._searchSubject$.next(value),
+      error => this._searchSubject$.error(error)
+    )
   }
 
 }
--- src/app/song/song.module.ts
+++ src/app/song/song.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { SongRoutingModule } from './song-routing.module';
 import { SongListComponent } from './song-list/song-list.component';
@@ -8,7 +10,10 @@ import { SongViewComponent } from './song-view/song-view.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -17,12 +22,17 @@ import { RouterModule } from '@angular/router';
     SongViewComponent
   ],
   imports: [
+    ReactiveFormsModule,
     RouterModule,
     CommonModule,
     SongRoutingModule,
     MatCardModule,
     MatToolbarModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class SongModule { }
