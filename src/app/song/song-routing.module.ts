2c4b0035cf3505ea2aad85b40a127323e809a3a9
wojtas1411 <wojtas.t.1997@gmail.com>
2022-02-15 12:59:48 +0100

    add help page

--- src/app/song/song-routing.module.ts
+++ src/app/song/song-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelpComponent } from './help/help.component';
 import { SongListComponent } from './song-list/song-list.component';
 import { SongViewComponent } from './song-view/song-view.component';
 
 const routes: Routes = [
   {path: '', component: SongListComponent},
   {path: 'song/:id', component: SongViewComponent},
+  {path: 'help', component: HelpComponent}
 ];
 
 @NgModule({
