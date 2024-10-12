202b99d277a6860461f1d080c360a5c7aae8d31a
Wojtek <wojtas.t.1997@gmail.com>
2024-10-12 21:31:48 +0200

    remove old deployment files

--- make-deployment.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p docs
-npm run build
-(cd docs ; rm -rf assets *.html *.js *.json *.css *.ico *.webmanifest)
-cp -r dist/spiewnigator-angular/browser/* docs
-cp docs/index.html docs/404.html
