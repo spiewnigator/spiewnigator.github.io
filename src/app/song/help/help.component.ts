3e019b1b61a73d9f2d59d3dc484ad7467e6ceb6c
wojtai <wojtas.t.1997@gmail.com>
2025-12-30 20:50:54 +0100

    service that checks for updates

--- src/app/song/help/help.component.ts
+++ src/app/song/help/help.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
 import { Observable, from } from 'rxjs';
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { AppUpdateService } from 'src/app/service/app-update.service';
 
 @Component({
     selector: 'app-help',
     templateUrl: './help.component.html',
     styleUrls: ['./help.component.scss'],
-    imports: [MatToolbar, MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, DatePipe]
+    imports: [MatToolbar, MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, DatePipe, AsyncPipe]
 })
 export class HelpComponent implements OnInit {
 
-    buildTimestamp?: Date;
+    private readonly appUpdateService = inject(AppUpdateService);
+
+    public buildTimestamp?: Date;
+
+    public isUpdateAvailableMessage$ = this.appUpdateService.isUpdateAvailable$();
 
     ngOnInit(): void {
         this.getBuildTimestamp().subscribe(ts => {
