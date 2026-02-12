e37aeae162345a2ea17c4de8810cfae83a7f0cb9
Wojtek <wojtas.t.1997@gmail.com>
2026-02-12 19:01:12 +0100

    adjust script

--- bump_packages.sh
+++ bump_packages.sh
@@ -3,12 +3,11 @@ set -e
 ANGULAR_VERSION=21
 
 # step 1: checkout new branch and save it's name
-# BRANCH_NAME="bump_packages_$(date +%Y%m%d%H%M%S)"
+BRANCH_NAME="bump_packages_$(date +%Y%m%d)"
 
-# git checkout -b $BRANCH_NAME
+git checkout -b $BRANCH_NAME
 
 # step 2: update angular packages
-# Update Angular packages
 npx ng update @angular/core@$ANGULAR_VERSION @angular/cli@$ANGULAR_VERSION @angular-eslint/schematics@$ANGULAR_VERSION
 
 # step 3: commit changes
@@ -31,4 +30,4 @@ git add .
 git commit --amend --no-edit
 
 # step 8: push branch
-# git push origin HEAD
+git push origin HEAD
